8074fdcfa4d2597c97bbdd6325bb09c710cd9209
Hiborei <hiborei23@gmail.com>
2020-05-06 16:02:39 +0200

    Logika gry

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ background = pygame.image.load("bgd.png")
 board = pygame.image.load("board.png")
 
 pygame.display.set_icon(pawn_w)
+phase = 0
+
+pawns_count = [0, 0]
+first_mill = 0
+
+turn = 1
+win_player = 0
 
 
 class Point:
@@ -160,8 +167,72 @@ def switch_position(p1, p2):
 
 
 def remove_piece(p):
+    global phase, win_player
+    state = p.get_state()
+    pawns_count[state - 1] = pawns_count[state - 1] - 1
+    if pawns_count[state-1] == 2:
+
+        win_player = turn
     p.change_state(0)
     redraw_board()
+    pygame.display.flip()
+
+
+def switch_turn():
+    global turn
+    if turn == 1:
+        turn = 2
+    else:
+        turn = 1
+    print("Now it's player " + str(turn) + " turn")
+
+
+def check_for_mills(p: Point):
+    player = p.get_state()
+    l, x, y = point.get_l_x_y()
+    mill = True
+    if layers[1, x, y]:
+        for i in range(0, 3):
+            if layers[1, x, y].is_neighbor(layers[i, x, y]) or i == 1:
+                if not layers[i, x, y].get_state() == player:
+                    mill = False
+            else:
+                mill = False
+        if mill:
+            return True
+    mill = True
+    if layers[l, 1, y]:
+        for i in range(0, 3):
+            if layers[l, 1, y].is_neighbor(layers[l, i, y]) or i == 1:
+                if not layers[l, i, y].get_state() == player:
+                    mill = False
+            else:
+                mill = False
+        if mill:
+            return True
+    mill = True
+    if layers[l, x, 1]:
+        for i in range(0, 3):
+            if layers[l, x, 1].is_neighbor(layers[l, x, i]) or i == 1:
+                if not layers[l, x, i].get_state() == player:
+                    mill = False
+            else:
+                mill = False
+        if mill:
+            return True
+    return False
+
+
+def check_available_moves():
+    for l in layers:
+        for x in l:
+            for p in x:
+                if p:
+                    if p.get_state() == turn:
+                        for i in p.get_neighbors():
+                            if i.get_state() == 0:
+                                return True
+    return False
 
 
 if __name__ == "__main__":
@@ -178,21 +249,76 @@ if __name__ == "__main__":
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             if event.type == pygame.BUTTON_X1:
+                print('PHASE: '+str(phase))
                 pos = pygame.mouse.get_pos()
                 point = which_point(pos)
+                print(pawns_count)
+                if win_player > 0:
+                    print('KONIEC, wygral gracz ' + str(win_player))
                 if point:
-                    if x < 3:
-                        draw_on_point(point, x)
-                        pygame.display.flip()
-                        x = x+1
+                    if phase == 0:
+                        if point.get_state() == 0:
+                            draw_on_point(point, turn)
+                            pygame.display.flip()
+                            pawns_count[turn-1] = pawns_count[turn-1] + 1
+                            if first_mill == 0 and check_for_mills(point):
+                                first_mill = turn
+                                print('FOUND')
+                            switch_turn()
+                        if pawns_count[1] == 9:
+                            if first_mill == 0:
+                                phase = 2
+                                turn = 2
+                            else:
+                                phase = 1
+
                     else:
-                        if point.get_state() > 0:
-                            temp = point
+                        if phase == 1:
+                            if not first_mill == 3:
+                                turn = first_mill
+                            if not turn == point.get_state() and not point.get_state() == 0:
+                                remove_piece(point)
+                                switch_turn()
+                                if first_mill == 3:
+                                    phase = 2
+                                first_mill = 3
                         else:
+<<<<<<< Updated upstream
                             if point.is_neighbor(temp):
                                 switch_position(temp, point)
                                 temp = None
 
+=======
+                            if phase == 2:
+                                if point.get_state() == turn:
+                                    temp = point
+                                if point.get_state() == 0:
+                                    if point.is_neighbor(temp):
+                                        animated_switch_position(temp, point)
+                                        temp = None
+                                        if check_for_mills(point):
+                                            phase = 3
+                                        else:
+                                            switch_turn()
+                            else:
+                                if phase == 3:
+                                    if not turn == point.get_state():
+                                        remove_piece(point)
+                                        switch_turn()
+                                        phase = 2
+                                else:
+                                    if phase == 4:
+                                        if point.get_state() == turn:
+                                            temp = point
+                                        if point.get_state() == 0:
+                                            if point.is_neighbor(temp):
+                                                animated_switch_position(temp, point)
+                                                temp = None
+                                                switch_turn()
+                                                phase = 2
+                if phase == 2 and not check_available_moves():
+                    phase = 4
+>>>>>>> Stashed changes
             if event.type == pygame.KEYDOWN:
                 clear_screen()
 
